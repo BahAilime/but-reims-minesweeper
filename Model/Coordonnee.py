aa82003f80821d3c5c9ce450b05e71b2188bd0ea
Émilia Beguin <emilia.beguin@etudiant.univ-reims.fr>
2023-01-06 17:44:09 +0100

    Coordonnées implémentées : tests ok

--- Model/Coordonnee.py
+++ Model/Coordonnee.py
@@ -19,3 +19,42 @@ def type_coordonnee(coord: tuple) -> bool:
         and coord[0] >= 0 and coord[1] >= 0
 
 
+def construireCoordonnee(num_ligne: int, num_colonne: int) -> tuple:
+    """
+    Prends en paramètre les coordonées y puis x et renvoie un tuple de ses coordonées ce sont bien des entiers positifs
+
+    :param num_ligne: Coordonée Y
+    :param num_colonne: Coordonée X
+    :return: couple représentant le numéro de ligne et celui de la colonne (commençant les deux à 0)
+    """
+    if type(num_ligne) == float or type(num_colonne) == float:
+        raise TypeError(f"construireCoordonnee: Le numéro de ligne {type(num_ligne)} ou  "
+                        f"le  numéro  decolonne {type(num_colonne)} ne  sont pas des entiers")
+    elif num_ligne < 0 or num_colonne < 0:
+        raise ValueError(f"construireCoordonnee: Le  numéro de ligne {num_ligne} "
+                         f"ou de colonne {num_colonne} ne sont pas positifs")
+    return (num_ligne, num_colonne)
+
+def getLigneCoordonnee(coord: tuple) -> int:
+    """
+    Renvoie la valeur Y des coordonées donné en paramètre
+
+    :param coord: couple représentant le numéro de ligne et celui de la colonne (commençant les deux à 0)
+    :return: Y
+    """
+    if not type_coordonnee(coord):
+        raise TypeError("getLigneCoordonnee: Le paramètre n’est pas une coordonnée")
+
+    return coord[0]
+
+def getColonneCoordonnee(coord: tuple) -> int:
+    """
+    Renvoie la valeur X des coordonées donné en paramètre
+
+    :param coord: couple représentant le numéro de ligne et celui de la colonne (commençant les deux à 0)
+    :return: X
+    """
+    if not type_coordonnee(coord):
+        raise TypeError("getLigneCoordonnee: Le paramètre n’est pas une coordonnée")
+
+    return coord[1]
